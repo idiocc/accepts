85a2238c6ba39e9068b8bb33333cfa9434407d62
Anton <anton@adc.sh>
2019-05-11 11:31:24 +0300

    generate types

--- types/externs.js
+++ types/externs.js
@@ -8,45 +8,44 @@ var _goa = {}
 _goa.Accepts
 /**
  * Check if the given `type(s)` is acceptable, returning the best match when true, otherwise `false`, in which case you should respond with 406 "Not Acceptable".
-      The `type` value may be a single mime type string such as "application/json", the extension name such as "json" or an array `["json", "html", "text/plain"]`. When a list or array is given the _best_ match, if any is returned. _Examples_:
+      The `type` value may be a single mime type string such as "application/json", the extension name such as "json" or an array `["json", "html", "text/plain"]`. When a list or array is given the _best_ match, if any is returned. When no types are given as arguments, returns all types accepted by the client in the preference order. _Examples_:
       - [Accept: text/html]
       `this.types('html') => "html"`
-      - [Accept: text/*, application/json]
+      - [Accept: text/＊, application/json]
       `this.types('html') => "html"`
       `this.types('text/html') => "text/html"`
       `this.types('json', 'text') => "json"`
       `this.types('application/json') => "application/json"`
-      - [Accept: text/*, application/json]
+      - [Accept: text/＊, application/json]
       `this.types('image/png') => false`
       `this.types('png') => false`
-      - [Accept: text/*;q=.5, application/json]
+      - [Accept: text/＊;q=.5, application/json]
       `this.types(['html', 'json']) => "json"`
       `this.types('html', 'json') => "json"`
-      When no types are given as arguments, returns all types accepted by the client in the preference order. _For example_:
-      - [Accept: application/*;q=0.2, image/jpeg;q=0.8, text/html, text/plain]
-      `this.types() => ["text/html", "text/plain", "image/jpeg", "application/*""]`
+      - [Accept: application/＊;q=0.2, image/jpeg;q=0.8, text/html, text/plain]
+      `this.types() => ["text/html", "text/plain", "image/jpeg", "application/＊"]`
  * @type {function((string|!Array<string>)=, ...string): (string|!Array<string>|boolean)}
  */
 _goa.Accepts.prototype.types
 /**
  * Check if the given `type(s)` is acceptable, returning the best match when true, otherwise `false`, in which case you should respond with 406 "Not Acceptable".
-      The `type` value may be a single mime type string such as "application/json", the extension name such as "json" or an array `["json", "html", "text/plain"]`. When a list or array is given the _best_ match, if any is returned. _Examples_:
+      The `type` value may be a single mime type string such as "application/json", the extension name such as "json" or an array `["json", "html", "text/plain"]`. When a list or array is given the _best_ match, if any is returned. When no types are given as arguments, returns all types accepted by the client in the preference order. _Examples_:
       - [Accept: text/html]
       `this.types('html') => "html"`
-      - [Accept: text/*, application/json]
+      - [Accept: text/＊, application/json]
       `this.types('html') => "html"`
       `this.types('text/html') => "text/html"`
       `this.types('json', 'text') => "json"`
       `this.types('application/json') => "application/json"`
-      - [Accept: text/*, application/json]
+      - [Accept: text/＊, application/json]
       `this.types('image/png') => false`
       `this.types('png') => false`
-      - [Accept: text/*;q=.5, application/json]
+      - [Accept: text/＊;q=.5, application/json]
       `this.types(['html', 'json']) => "json"`
       `this.types('html', 'json') => "json"`
       When no types are given as arguments, returns all types accepted by the client in the preference order. _For example_:
-      - [Accept: application/*;q=0.2, image/jpeg;q=0.8, text/html, text/plain]
-      `this.types() => ["text/html", "text/plain", "image/jpeg", "application/*""]`
+      - [Accept: application/＊;q=0.2, image/jpeg;q=0.8, text/html, text/plain]
+      `this.types() => ["text/html", "text/plain", "image/jpeg", "application/＊"]`
  * @type {function((string|!Array<string>)=, ...string): (string|!Array<string>|boolean)}
  */
 _goa.Accepts.prototype.type
